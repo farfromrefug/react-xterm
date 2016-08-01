d969e5e99ff861af5dc1ee2116d19086cf7b5472
Martin Guillon <martin.guillon@akylas.fr>
2016-08-01 14:10:35 +0200

    definiton

---
+++ dist/index.d.ts
@@ -0,0 +1,36 @@
+import * as React from 'react';
+export interface IXtermProps {
+    onChange: React.PropTypes.func;
+    onInput: React.PropTypes.func;
+    onFocusChange: React.PropTypes.func;
+    onScroll: React.PropTypes.func;
+    options: React.PropTypes.object;
+    path: React.PropTypes.string;
+    value: React.PropTypes.string;
+    className: React.PropTypes.any;
+    xtermInstance: React.PropTypes.object;
+}
+export interface IXtermState {
+    isFocused: boolean;
+}
+export default class ReactXTerm extends React.Component<IXtermProps, IXtermState> {
+    xterm: any;
+    refs: {
+        [string: string]: any;
+        container: HTMLDivElement;
+    };
+    constructor(props?: IXtermProps, context?: any);
+    getXTermInstance(): any;
+    getInitialState(): {
+        isFocused: boolean;
+    };
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    getXTerm(): any;
+    focus(): void;
+    focusChanged(focused: any): void;
+    onInput(data: any): void;
+    layout(): void;
+    setCursorBlink(blink: boolean): void;
+    render(): JSX.Element;
+}
